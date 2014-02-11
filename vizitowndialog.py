ee9115befef92460e54dacd5ef8c6ab305eeafea
Camille TJHOA <camille.tjhoa@gmail.com>
2014-02-11 14:50:47 +0100

    Add doc to vizitowndialog

--- vizitowndialog.py
+++ vizitowndialog.py
@@ -33,6 +33,7 @@ from vt_as_app import VTAppServer
 from vt_as_providers import ProviderManager, PostgisProvider
 
 
+## Vizitown dialog in QGIS GUI
 class VizitownDialog(QtGui.QDialog, Ui_Vizitown):
 
     def __init__(self):
@@ -42,17 +43,19 @@ class VizitownDialog(QtGui.QDialog, Ui_Vizitown):
         self.appServer = None
         self.appServerRunning = False
 
+    ## Behavior whit a close event
     def closeEvent(self, QCloseEvent):
         if self.appServer:
             self.appServer.stop()
 
-    # Set the default extent
+    ## Set the default extent
     def initExtent(self, extent):
         self.Xmin.setText("%.4f" % extent.xMinimum())
         self.Ymin.setText("%.4f" % extent.yMinimum())
         self.Xmax.setText("%.4f" % extent.xMaximum())
         self.Ymax.setText("%.4f" % extent.yMaximum())
-        # Set the values of the taile by default
+
+        ## Set the values of the taile by default
         self.cb_tuile.clear()
         self.cb_tuile.addItem('256 x 256')
         self.cb_tuile.addItem('512 x 512')
@@ -61,26 +64,30 @@ class VizitownDialog(QtGui.QDialog, Ui_Vizitown):
         self.cb_tuile.addItem('4096 x 4096')
         self.cb_tuile.setCurrentIndex(1)
 
+    ## Reset all widgets
     def clearListWidget(self):
         self.cb_MNT.clear()
         self.cb_Raster.clear()
         self.listWidget_Left.clear()
         self.listWidget_Right.clear()
 
+    ## Return True if the layer is a DEM ?? which come from a database
     def isDem(self, layer):
         return (layer.type() == QgsMapLayer.RasterLayer and
                 layer.providerType() == "gdal" and
                 layer.bandCount() == 1) and not layer.source().startswith('dbname')
 
+    ## Return True if the layer is a Raster which come from a database
     def isRaster(self, layer):
         return (layer.type() == QgsMapLayer.RasterLayer and
                 layer.providerType() == "gdal" and
                 layer.bandCount() >= 3) and not layer.source().startswith('dbname')
 
+    ## Return True if the layer is a Vector which come from a database
     def isVector(self, layer):
         return (layer.type() == QgsMapLayer.VectorLayer) and layer.source().startswith('dbname')
 
-    # Add layer in combobox and listWidget
+    ## Add layer in combobox and listWidget
     def loadLayers(self):
         self.clearListWidget()
         layerListIems = QgsMapLayerRegistry().instance().mapLayers().items()
@@ -93,20 +100,20 @@ class VizitownDialog(QtGui.QDialog, Ui_Vizitown):
             if self.isRaster(layer):
                 self.cb_Raster.addItem(layer.name(), id)
 
-    # Add vector layer in a right listView
+    ## Add vector layer in a right listView
     def on_but_Add_released(self):
         self.listWidget_Right.addItem(self.listWidget_Left.takeItem(self.listWidget_Left.currentRow()))
 
-    # Remove vector layer in a right listView
+    ## Remove vector layer in a right listView
     def on_but_Supp_released(self):
         self.listWidget_Left.addItem(self.listWidget_Right.takeItem(self.listWidget_Right.currentRow()))
 
-    # Set the tab advanced option by default
+    ## Set the tab advanced option by default
     def on_but_defaut_released(self):
         self.Numero_Port.setText("8888")
         self.cb_tuile.setCurrentIndex(1)
 
-    # Run the 3D scene
+    ## Generate and launch the rendering of the 3D scene
     def on_btnGenerate_released(self):
         if self.appServerRunning:
             self.btnGenerate.setText("Server is stopping")
@@ -121,10 +128,12 @@ class VizitownDialog(QtGui.QDialog, Ui_Vizitown):
             self.openWebBrowser()
             self.appServerRunning = True
 
+    ## Open the default web browser
     def openWebBrowser(self):
         url = 'http://localhost:8888'
         webbrowser.open(url)
 
+    ## Create all providers with the selected layers in the GUI
     def createProviders(self):
         for i in range(self.listWidget_Right.count()):
             vectorLayer = self.listWidget_Right.item(i).data(QtCore.Qt.UserRole)
