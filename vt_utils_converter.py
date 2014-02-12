2d6c04d559d2d89672b034631dff1135a56cbb0d
Mathieu CATTIN <panach25@gmail.com>
2014-02-12 01:11:08 +0100

    Final Utils Doc

--- vt_utils_converter.py
+++ vt_utils_converter.py
@@ -2,9 +2,10 @@ import re
 from xml.dom import minidom
 
 
-## Converts an X3D formats in a json
+## X3DTranslateToThreeJs class to converts an X3D formats in a json
 class X3DTranslateToThreeJs:
 
+    ## The Constructor
     def __init__(self):
         self.nbFaces = 0
         self.nbVertices = 0
@@ -48,6 +49,10 @@ class X3DTranslateToThreeJs:
 
 }"""
 
+    ## parse method to manage the process in the class and 
+    #  use the appropriate process in function of the data
+    #  @param message to stock the data
+    #  @param geometry to check the type of geometry
     def parse(self, message, geometry):
 
         if geometry == 'POINT':
@@ -61,6 +66,9 @@ class X3DTranslateToThreeJs:
             else:
                 return None
 
+    ## __parse_point method to parse a point data
+    #  @param message to stock the data
+    #  @return a json file
     def __parse_point(self, message):
         nbFace = 0
         nbPoint = 1
@@ -68,6 +76,9 @@ class X3DTranslateToThreeJs:
         vertice = re.sub(' ', ',', message)
         return self.__get_json(nbFace, nbPoint, None, vertice)
 
+    ## __parse_line method to parse a line data
+    #  @param xmldoc to stock the data
+    #  @return a json file
     def __parse_line(self, xmldoc):
         nbFace = 0
 
@@ -76,6 +87,9 @@ class X3DTranslateToThreeJs:
 
         return self.__get_json(nbFace, nbVertice, None, vertices)
 
+    ## __parse_triangle method to parse a triangle data
+    #  @param xmldoc to stock the data
+    #  @return a json file
     def __parse_triangle(self, xmldoc):
         nbIndexFace = 3
         bitMask = 0
@@ -94,6 +108,9 @@ class X3DTranslateToThreeJs:
         nbFace = nbVertice / nbIndexFace
         return self.__get_json(nbFace, nbVertice, faces, vertices)
 
+    ## __get_vertices Getter of vertex data
+    #  @param xmldoc to stock the data
+    #  @return a specific vertex
     def __get_vertices(sef, xmldoc):
         nodeVertice = xmldoc.getElementsByTagName('Coordinate')
         vertices = nodeVertice[0].getAttribute('point')
@@ -102,10 +119,19 @@ class X3DTranslateToThreeJs:
             vertices = vertices[:-1]
         return vertices
 
+    ## __count_vertice method to count the number of vertex
+    #  @param vertices stock a vertex number
+    #  @return a number of vertex
     def __count_vertice(self, vertices):
         verticesTab = vertices.split(',')
         return len(verticesTab) / self.nbPointVertice
 
+    ## __get_json Getter of json data
+    #  @param nbFaces to define the number of faces
+    #  @param nbVertices to define the number of vertex
+    #  @param faces stock a faces values
+    #  @param vertices stock a vertex values
+    #  @return a json file
     def __get_json(self, nbFaces, nbVertices, faces, vertices):
         self.__define_field(nbFaces, nbVertices)
 
@@ -122,6 +148,9 @@ class X3DTranslateToThreeJs:
 
         return json
 
+    ## __define_field method define information about data
+    #  @param nbFaces to define the number of faces
+    #  @param nbVertices to define the number of vertex
     def __define_field(self, nbFaces, nbVertices):
         self.nbFaces = nbFaces
         self.nbVertices = nbVertices
