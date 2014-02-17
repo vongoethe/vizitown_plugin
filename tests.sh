e86654a169e37dc33345e3b262e81be1813a767e
Louis POIGNANT <poignant.louis@gmail.com>
2014-02-17 16:57:26 +0100

    Remove test_provider from tests.sh

--- tests.sh
+++ tests.sh
@@ -3,11 +3,9 @@
 if hash python2 2>/dev/null; then
 	python2 vt_test_converter.py
 	python2 vt_test_parser.py
-	python2 vt_test_providers.py
 	python2 vt_test_tiler.py
 else
 	python vt_test_converter.py
 	python vt_test_parser.py
-	python vt_test_providers.py
 	python vt_test_tiler.py
 fi
