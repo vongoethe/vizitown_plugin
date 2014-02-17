c8c6d16fd11ee8ca66442b1cfaea801e486ba137
Louis POIGNANT <poignant.louis@gmail.com>
2014-02-17 11:59:48 +0100

    Return empty JS if no vectors to returns

--- vt_as_handlers.py
+++ vt_as_handlers.py
@@ -1,6 +1,6 @@
 import json
 import cyclone.websocket
-from vt_utils_converter import X3DTranslateToThreeJs
+from vt_utils_converter import PostgisToJSON
 from vt_as_provider_manager import ProviderManager
 from vt_as_sync import SyncManager
 
@@ -29,16 +29,18 @@ class DataHandler(cyclone.websocket.WebSocketHandler):
     #  '{"Xmin": 0, "Ymin": 0, "Xmax": 50, "Ymax": 50}'
     def messageReceived(self, message):
         bufferSize = 100
-        print message
         d = json.loads(message)
         vectors = ProviderManager.instance().request_tile(d['Xmin'], d['Ymin'], d['Xmax'], d['Ymax'])
-        translator = X3DTranslateToThreeJs()
+        if not vectors:
+            self.sendMessage("{}")
+            return
+
+        translator = PostgisToJSON()
         for v in vectors:
             ## TODO: Maybe make a buffer
             array = []
             while v['it'].next():
                 # seconde boucle
-                print "sendmessage"
                 for i in range(bufferSize):
                     if v['hasH']:
                         array.append([v['it'].value(0), v['it'].value(1)])
@@ -48,7 +50,6 @@ class DataHandler(cyclone.websocket.WebSocketHandler):
                     if not v['it'].next():
                         break
                 json_ = translator.parse(array, v['geom'], v['hasH'])
-                print json_
                 self.sendMessage(json_)
 
     ## Method call when the websocket is closed
