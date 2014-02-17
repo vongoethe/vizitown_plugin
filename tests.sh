4087afc2b444ebeac74948e6a7764226835e32e2
Louis POIGNANT <poignant.louis@gmail.com>
2014-02-17 16:58:41 +0100

    Remove test_tiler from tests.sh

--- tests.sh
+++ tests.sh
@@ -3,9 +3,7 @@
 if hash python2 2>/dev/null; then
 	python2 vt_test_converter.py
 	python2 vt_test_parser.py
-	python2 vt_test_tiler.py
 else
 	python vt_test_converter.py
 	python vt_test_parser.py
-	python vt_test_tiler.py
 fi
