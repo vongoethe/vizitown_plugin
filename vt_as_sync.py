3d23e40b0b92282db0400e2055a05ff6650ff627
Louis POIGNANT <poignant.louis@gmail.com>
2014-03-03 12:04:31 +0100

    Add presentation text

--- vt_as_sync.py
+++ vt_as_sync.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ Vizitown
+                                 A QGIS plugin
+ QGIS Plugin for viewing data in 3D
+                              -------------------
+        begin                : 2014-02-03
+        copyright            : (C) 2014 by Cubee(ESIPE)
+        email                : lp_vizitown@googlegroups.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 import json
 from vt_utils_singleton import Singleton
 
